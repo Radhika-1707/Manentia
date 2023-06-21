6302a7ad3522e60c03750475637c63e5fb2c0ac7
Radhika-1707 <radhikapullu17@gmail.com>
2023-06-22 03:09:13 +0530

    fixes build error

--- src/app/components/MiniHero/index.tsx
+++ src/app/components/MiniHero/index.tsx
@@ -7,7 +7,7 @@ function MiniHero({}: Props) {
     <div className='mt-[115px]'>
         <h1 className='text-[33px] font-bold text-[#252525] leading-[160%] text-center'>Lorem Ipsum is simply dummy text of the printing. </h1>
         <p className='pt-[32px] text-center text-[#868686] font-normal'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-        <br/> Lorem Ipsum has been the industry's</p>
+        <br/> Lorem Ipsum has been the industry`&apos;`s</p>
     </div>
   )
 }
